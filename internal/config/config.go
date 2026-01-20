b4fbadc80197af755a6e7cf1f789d1553b5be8c8
talonlikeaclaw <talonlikeaclaw@proton.me>
2026-01-20 15:08:12 -0500

    add config.go and implement config load function

---
+++ internal/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+func Load(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
